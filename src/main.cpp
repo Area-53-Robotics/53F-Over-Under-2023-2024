e5932ac199dc88c722e02645f24a8503998ba330
Krish <krishrp07@gmail.com>
2024-01-30 15:40:54 -0500

    .

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,7 @@
 #include <string>
 #include <sys/unistd.h>
 #include <unistd.h>
-//testing for save
-/**
- * A callback function for LLEMU's center button.
- *
- * When this callback is fired, it will toggle line 2 of the LCD text between
- * "I was pressed!" and nothing.
- */
+
 void on_center_button() {
 	static bool pressed = false;
 	pressed = !pressed;
@@ -22,67 +16,18 @@ void on_center_button() {
 	}
 }
 
-/**
- * Runs initialization code. This occurs as soon 
- * All other competition modes are blocked by initialize; it is recommended
- * to keep execution time for this mode under a few seconds.
- */
 void initialize() {
 	pros::lcd::initialize();
 	pros::lcd::register_btn1_cb(on_center_button);
 }
 
-/**
- * Runs while the robot is in the disabled state of Field Management System or
- * the VEX Competition Switch, following either autonomous or opcontrol. When
- * the robot is enabled, this task will exit.
- */
-void disabled() {}
+//------------------------------------------------------//
 
-
-
-/**
- * Runs after initialize(), and before autonomous when connected to the Field
- * Management System or the VEX Competition Switch. This is intended for
- * competition-specific initialization routines, such as an autonomous selector
- * on the LCD.
- * 
- * This task will exit when the robot is enabled and autonomous or opcontrol
- * starts.
- */
-void competition_initialize() {}
-
-/**
- * Runs the operator control code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the operator
- * control mode.
- *huhuhuhuhuhuhuhuhuh
- * If no competition control is connected, this function will run immediately
- * following initialize().
- *
- * If the robot is disabled or communications is lost, the
- * operator control task will be stopped. Re-enabling the robot will restart the
- * task, not resume it from where it left off.
- */
 void opcontrol() {
 //Variables
-  pros::Motor_Group Reset ({1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20});
-  bool ResetSwitch = false;
-
   pros::Controller master(pros::E_CONTROLLER_MASTER);
   pros::Motor_Group LDriveTrain ({-19, -20, -6});
   pros::Motor_Group RDriveTrain ({8, 9, 10});
-
-  int LDriveTrainTEMP;
-  int RDriveTrainTEMP;
-  pros::Motor L1 {20};
-  pros::Motor L2 {6};
-  pros::Motor L3 {19};
-  pros::Motor R1 {8};
-  pros::Motor R2 {9};
-  pros::Motor R3 {10};
-
 
   bool toggleDrive = false;
   int toggleIntake = 0;
@@ -106,16 +51,6 @@ void opcontrol() {
       RDriveTrain.move(-master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y));
       LDriveTrain.move(-master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y));
    }
-
-//Motor Temprature
-    //Left DriveTrain
-      //LDriveTrainTEMP = (L1.get_temperature() + L2.get_temperature() + L3.get_temperature()) / 3; 
-      //master.set_text(0, 25, "hi");
-    //Right DriveTrain
-      //RDriveTrainTEMP = (R1.get_temperature() + R2.get_temperature() + R3.get_temperature()) / 3; 
-      //master.set_text(1, 0, "Hello");
-    //Cata
-      
 
 //Intake (not main funtion)
     if (master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_R1)) {
@@ -151,22 +86,6 @@ void opcontrol() {
     }else {
       InTake.move(0);
     }
-
-
-//Reset switch
-    if(master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_DOWN)) {
-      ResetSwitch = !ResetSwitch;
-    }
-
-    if (ResetSwitch == true) {
-      Reset.move(0);
-      pros::lcd::set_text(1, "PAUSE MOTORS --> ON (Down)");
-    }else {
-      ResetSwitch = false;
-      pros::lcd::set_text(1, "PAUSE MOTORS --> OFF (Down)");
-    }
-
-//Small animation
 
 		pros::delay(10);
 
